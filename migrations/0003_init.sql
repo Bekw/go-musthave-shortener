39833593a3db328e879acbc5613e39ef4feef02e
berekeqb <b.konyrbayev@mk.int>
2025-11-24 16:00:56 +0500

    Increment 15 правки

--- migrations/0003_init.sql
+++ migrations/0003_init.sql
@@ -2,8 +2,7 @@
 CREATE TABLE IF NOT EXISTS user_urls (
     user_id TEXT NOT NULL,
     url_id  TEXT NOT NULL,
-    PRIMARY KEY (user_id, url_id),
-    FOREIGN KEY (url_id) REFERENCES urls(id)
+    PRIMARY KEY (user_id, url_id)
 );
 CREATE INDEX IF NOT EXISTS idx_user_urls_user_id ON user_urls(user_id);
 
